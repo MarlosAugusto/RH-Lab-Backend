7a29d40c49cd3cf7fdfc2d4a3f8d3b33dc4c91b2
Marlos Augusto <marlosaugusto@outlook.com>
2019-08-21 10:03:13 -0300

    refactoring to test connect to db

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 const express = require('express')
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const dotenv = require('dotenv')
 
 dotenv.config()
 
 const app = express()
 
-const client = new Client({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true
 });
 
-client.connect();
-
-app.post("/test", (req, res) => {
+app.post("/test", async (req, res) => {
   console.log('url',process.env.DATABASE_URL)
-  console.log('teste')
-  client.query('SELECT now();', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT NOW();');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
   
   return res.json({ hello: "World" })
 })
